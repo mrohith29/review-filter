72a45dbeb08a396b2e58164b7f7dd4af0cdee891
mrohith29 <mariyalarohith29@gmail.com>
2024-06-17 11:17:19 +0530

    text pre-processing and machine conversion done

--- language translation/translation.py
+++ language translation/translation.py
@@ -1,9 +1,9 @@
 import googletrans as gt
 
-def confidance(word, translator):
+def confidence(word, translator):
     detection = translator.detect(word)
     return detection.lang
-    
+
 def translate_text(text, translator, src_lang, dest_lang='en'):
     translation = translator.translate(text, src=src_lang, dest=dest_lang)
     return translation.text
@@ -13,9 +13,22 @@ with open('input.txt', 'r', encoding='utf-8') as input_file, open('output.txt', 
     for line in input_file:
         words = line.split()
         translated_line = []
-        for word in words:
-            language = confidance(word, translator)
 
+        filtered_data = []
+        emt_str = words[0]
+
+        for i in range(1, len(words)):
+            if confidence(words[i], translator) == confidence(words[i-1], translator):
+                emt_str += ' ' + words[i]
+            else:
+                if emt_str:
+                    filtered_data.append(emt_str)
+                emt_str = words[i]
+        if emt_str:
+            filtered_data.append(emt_str)
+
+        for word in filtered_data:
+            language = confidence(word, translator)
             if language == 'te':
                 new_word = translate_text(word, translator, 'te')
                 translated_line.append(new_word)
